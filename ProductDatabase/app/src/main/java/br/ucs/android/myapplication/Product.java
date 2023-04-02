461d4b087def32cd613b9265faedbf3ad9755959
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-04-02 09:53:51 -0300

    Created the class product

---
+++ ProductDatabase/app/src/main/java/br/ucs/android/myapplication/Product.java
@@ -0,0 +1,50 @@
+package br.ucs.android.myapplication;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+
+    public Product(){}
+
+    public Product(int id, String name, String description, double price){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
